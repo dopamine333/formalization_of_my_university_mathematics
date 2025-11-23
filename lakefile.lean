5dcf9b12bc83410e8cc0c4a1648eaae2d61a45f6
dopamine333 <austin20050819@gmail.com>
2025-11-23 22:52:27 +0800

    test lean blueprint "... After running this creation script, you can push to GitHub and wait for GitHub Actions to build your blueprint. ..."

--- lakefile.lean
+++ lakefile.lean
@@ -18,3 +18,9 @@ require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v
 @[default_target]
 lean_lib «FormalizationOfMyUniversityMathematics» where
   -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
